c49f98a5855875c2e85c1188333eb3c1fe95e06f
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-09-28 22:53:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from utils import load_model
+from .utils import load_model
 from explainer import explain_transaction
 
 # Load model
